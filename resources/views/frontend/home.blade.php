776ed73edbcedbc114cc48cd936b88f506e48cb0
kenzofa <susilo2305@gmail.com>
2024-07-16 10:41:18 +0700

    Update 2024-07-16

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -1,4 +1,4 @@
-<div style="height: 70px"></div> 
+<div style="height: 70px">test</div> 
 @foreach($landing_page as $_landing_page)
 	@include('frontend.'.$_landing_page->widget->target)
 @endforeach
